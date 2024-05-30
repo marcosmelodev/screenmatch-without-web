c2863bdc7848085ef4d3d95279f7f390e95da753
Marcos Melo <marcos.dhl@hotmail.com>
2024-05-30 19:33:24 -0300

    integrando API Chat GPT

--- src/main/java/br/com/alura/screenmatchpt/screenmatchpt/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatchpt/screenmatchpt/principal/Principal.java
@@ -2,12 +2,15 @@ package br.com.alura.screenmatchpt.screenmatchpt.principal;
 
 import br.com.alura.screenmatchpt.screenmatchpt.model.DadosSerie;
 import br.com.alura.screenmatchpt.screenmatchpt.model.DadosTemporada;
+import br.com.alura.screenmatchpt.screenmatchpt.model.Serie;
 import br.com.alura.screenmatchpt.screenmatchpt.service.ConsumoApi;
 import br.com.alura.screenmatchpt.screenmatchpt.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -15,39 +18,48 @@ public class Principal {
     private ConverteDados conversor = new ConverteDados();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=96433339";
+    private List<DadosSerie> dadosSeries = new ArrayList<>();
 
     public void exibeMenu(){
-        var menu = """
-                1 - Buscar séries
-                2 - Buscar episódios
-                
-                0 - Sair
-                
-                Escolha uma das opçoes: 
-                """;
+        var opcao = -1;
+        while (opcao != 0) {
+            var menu = """
+                    1 - Buscar séries
+                    2 - Buscar episódios
+                    3 - Listar séries
+                                    
+                    0 - Sair
+                                    
+                    Escolha uma das opçoes: 
+                    """;
 
-        System.out.println(menu);
-        var opcao = leitura.nextInt();
-        leitura.nextLine();
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+            leitura.nextLine();
 
-        switch (opcao) {
-            case 1:
-                buscarSerieWeb();
-                break;
-            case 2:
-                buscarEpisodioPorSerie();
-                break;
-            case 0:
-                System.out.println("Saindo...");
-                break;
-            default:
-                System.out.println("Opção inválida.");
+            switch (opcao) {
+                case 1:
+                    buscarSerieWeb();
+                    break;
+                case 2:
+                    buscarEpisodioPorSerie();
+                    break;
+                case 3:
+                    listarSeries();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
         }
     }
 
 
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
+        dadosSeries.add(dados);
         System.out.println(dados);
     }
 
@@ -71,6 +83,14 @@ public class Principal {
         temporadas.forEach(System.out::println);
     }
 
-
+    private void listarSeries() {
+        List<Serie> series = new ArrayList<>();
+        series = dadosSeries.stream()
+                        .map(d -> new Serie(d))
+                                .collect(Collectors.toList());
+        series.stream()
+                .sorted(Comparator.comparing(Serie::getGenero))
+                .forEach(System.out::println);
+    }
 
 }
